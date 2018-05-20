90d8c6a676dc66be03b835c37a8d9971ef3a343e
Antonio <retonho@gmail.com>
2018-05-20 21:31:41 +0200

    Créditos

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-// Author page.
-router.get('/author', (req, res, next) => {
-    res.render('author');
+// Credits page.
+router.get('/credits', (req, res, next) => {
+    res.render('credits');
 });
 
 module.exports = router;
