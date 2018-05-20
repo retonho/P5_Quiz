ed042795a328f0cc7bc65b2b0b790a023ac69f58
Antonio <retonho@gmail.com>
2018-05-20 21:15:36 +0200

    Primer commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// Author page.
+router.get('/author', (req, res, next) => {
+    res.render('author');
+});
+
+module.exports = router;
